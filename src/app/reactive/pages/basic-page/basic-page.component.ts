e81c6ddc2ea2f74cf3f0f6f8985754bc9a640348
Manuel <x.fuentealba.ortiz@gmail.com>
2023-12-01 01:42:23 -0300

    Formulario con validacion

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder,Validators } from '@angular/forms';
+
+const rtx5090={
+  name:'rtx 5090',
+  price:'2500',
+  inStorage:'5'
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
+
+  // public myform: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0)
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required,Validators.minLength(3)]],
+    price: [0,[Validators.required,Validators.min(0)]],
+    inStorage: [0,[Validators.required,Validators.min(0)]],
+  })
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5090)
+  }
+
+  isValidField(field: string):boolean | null{
+    return this.myForm.controls[field].errors
+     && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field: string ):string | null{
+
+    if(!this.myForm.controls[field])return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return'Este campo es requerido';
+
+          case 'minlength':
+            return `Minimo ${errors['minlength'].requiredLength} caracters.`
+      }
+    }
+    return null
+  }
+
+  onSave():void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return
+    };
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({price:0, inStorage:0})
+  }
 
 }
+
